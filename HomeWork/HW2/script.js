1118ed17f9452aaa2d595286ac494b91488edba8
AsiaCht <anastasiya.chubata@gmail.com>
2022-10-23 22:50:03 +0300

    Homework3

--- HomeWork/HW2/script.js
+++ HomeWork/HW2/script.js
@@ -1,30 +1,30 @@
-// let a = 8;
-// let b = 9; 
-// let rezult = (a+b>4) ? 'Мало' : 'Много';
-// console.log(rezult);
+let a = 8;
+let b = 9; 
+let rezult = (a+b>4) ? 'Мало' : 'Много';
+console.log(rezult);
 
-// login = 'Violet';
-// let massage = (login == 'Violet') ? 'Hey' :
-// (login == 'Boss') ? 'Hello' :
-// (login == '')? 'Undefind login' : 
-// '';
-// console.log(massage);
+login = 'Violet';
+let massage = (login == 'Violet') ? 'Hey' :
+(login == 'Boss') ? 'Hello' :
+(login == '')? 'Undefind login' : 
+'';
+console.log(massage);
 // прямокутник
-// for(let i = 0; i < 8; i++){
-//   for(let j = 0; j < 30; j++){
-//     document.write('*');
-//   }
-//   document.write('<br>');
-// }
+for(let i = 0; i < 8; i++){
+  for(let j = 0; j < 30; j++){
+    document.write('*');
+  }
+  document.write('<br>');
+}
 // прямокутний трикутник
-// for(let i = 0; i < 25; i++){
-//   for(let j = 0; j < i; j++){
-//     document.write('*');
-//   }
-//   document.write('<br>')
-// }
+for(let i = 0; i < 25; i++){
+  for(let j = 0; j < i; j++){
+    document.write('*');
+  }
+  document.write('<br>')
+}
 // трикутник
-/*for(let i = 0; i < 10; ++i){
+for(let i = 0; i < 10; ++i){
   for(let spase = (10-i); spase >0; spase-- ){
     document.write('&nbsp;');
   }
@@ -32,34 +32,34 @@
     document.write('*');
   }
    document.write('<br>');
-}*/
+}
 // ромб
-// for(let i = 0; i < 10; ++i){
-//   for(let spase = (10-i); spase > 0; spase--){
-//     document.write('&nbsp;');
-//   }
-//   for(let j = 0; j<=i; j++){
-//     document.write('*')
-//   }
-//   document.write('<br>');
-// }
-// for(let i = 0; i < 10; ++i){
-//   for(let j = 0; j<=i; j++){
-//     document.write('&nbsp;')
-//   }
-//   for(let spase = (10-i); spase >0; spase--){
-//     document.write('*');
-//   }
-//   document.write('<br>');
-// }
+for(let i = 0; i < 10; ++i){
+  for(let spase = (10-i); spase > 0; spase--){
+    document.write('&nbsp;');
+  }
+  for(let j = 0; j<=i; j++){
+    document.write('*')
+  }
+  document.write('<br>');
+}
+for(let i = 0; i < 10; ++i){
+  for(let j = 0; j<=i; j++){
+    document.write('&nbsp;')
+  }
+  for(let spase = (10-i); spase >0; spase--){
+    document.write('*');
+  }
+  document.write('<br>');
+}
 
-// const styles =['Джаз', 'Блюз'];
-// styles.push('Рок-н-рол');
-// console.log(styles);
-// styles.splice(2,0,'Класика');
-// console.log(styles);
-// styles.shift();
-// console.log(styles);
-// styles.unshift('Реп', 'Реггі');
-// console.log(styles);
+const styles =['Джаз', 'Блюз'];
+styles.push('Рок-н-рол');
+console.log(styles);
+styles.splice(2,0,'Класика');
+console.log(styles);
+styles.shift();
+console.log(styles);
+styles.unshift('Реп', 'Реггі');
+console.log(styles);
 
